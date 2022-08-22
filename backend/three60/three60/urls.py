20c97f6f385547444a1d89a1fff7fbeee592b2ef
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-08-22 17:32:28 +0100

    tweaked app for heroku

--- backend/three60/three60/urls.py
+++ backend/three60/three60/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from three60 import authentication
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/", include(authentication.urls)),
+    path('accounts/', include('allauth.urls')),
+    path("api/auth/", include('authentication.urls')),
 ]
