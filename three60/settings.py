8c19c27ced0210629542d1813c47bbb9d491069d
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-08-27 14:40:34 +0100

    swagger& redoc docs added

--- three60/settings.py
+++ three60/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "gmailapi_backend",
+    "drf_yasg",
     #all_auth apps
     'allauth',
     'allauth.account',
--- three60/urls.py
+++ three60/urls.py
@@ -15,10 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="three60 API",
+        default_version='1.0.0',
+        description="API documentation of three60",
+    ),
+    public=True,
+)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('accounts/', include('allauth.urls')),
     path("api/auth/", include('authentication.urls')),
 ]
