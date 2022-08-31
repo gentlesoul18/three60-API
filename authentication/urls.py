cbdc8cf913f0a3f50e421750ab373a063c92bc23
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-08-31 23:39:38 +0100

    authentication done

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import LoginView, RegisterView, VerifyEmailView
+from .views import LoginView, RegisterView, VerifyEmailView, GoogleLoginApi, UserApi, UserInitApi
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
-    path('signin',LoginView.as_view(), name='signin')
+    path('register', RegisterView.as_view(), name='register'),
+    path('verify-email', VerifyEmailView.as_view(), name='verify-email'),
+    path('signin',LoginView.as_view(), name='signin'),
+    path('google-login/', GoogleLoginApi.as_view(),name='google-login' ),
+    path('user/', UserApi.as_view(), name='user'),
+    path('init/', UserInitApi.as_view(), name='init'),
 ]
--- three60/urls.py
+++ three60/urls.py
@@ -32,6 +32,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('social/', include('social.urls')),
-    path('api/auth/', include('authentication.urls'))
+    path('auth/', include('authentication.urls'), name = 'auth'),
 ]
