e3a242052ddac85d05679b94840a8eb611cc2ae2
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-09-06 15:50:06 +0100

    postgres settings

--- three60/settings.py
+++ three60/settings.py
@@ -131,15 +131,21 @@ AUTH_USER_MODEL = "authentication.User"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": config("USER"),
+#         "USER": "postgres",
+#         "PASSWORD": config("PASSWORD"),
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("USER"),
-        "USER": "postgres",
-        "PASSWORD": config("PASSWORD"),
-        "HOST": "*",
-        "PORT": "5432",
-    }
+    "default": dj_database_url.config(
+        default="postgres://postgres:oyeladunkola@localhost:5432/three60", conn_max_age=600
+    )
 }
 
 # Password validation
