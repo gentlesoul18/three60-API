3cef263ab9687e9e1a99932f8aca716c1348bc46
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-09-17 12:49:38 +0100

    database migrated

--- three60/settings.py
+++ three60/settings.py
@@ -129,23 +129,23 @@ AUTH_USER_MODEL = "authentication.User"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('USER'),
-#         'USER': 'postgres',
-#         'PASSWORD': config('PASSWORD'),
-#         'HOST': 'localhost',
-#         'PORT': '5432', 
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('USER'),
+        'USER': 'postgres',
+        'PASSWORD': config('PASSWORD'
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
