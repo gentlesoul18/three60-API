c32efe97087fc2ec3a44b300f64c80ec919ef796
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-10-12 17:20:21 +0100

    Changed registration method

--- authentication/selectors.py
+++ authentication/selectors.py
@@ -14,3 +14,43 @@ def jwt_response_payload_handler(token, user=None, request=None):
         'token': token,
         'user': get_user(user=user),
     }
+
+
+ # token = RefreshToken.for_user(user).access_token
+        # current_site = get_current_site(request).domain #get the site the app is on currently
+        # relative_url = reverse('verify-email')
+        # absolute_url  = "http://" + current_site + relative_url + "?token=" + str(token)
+        # body = f"Hi {user.username}, verify your email with  this link \n {absolute_url}"
+        # subject = 'email verification'
+        # from_mail = 'devgentlesoul18@gmail.com'
+
+        # data = {'email_subject':subject, 'email_body':body, 'from_email':from_mail, 'to_email':[user.email]}
+
+        # send_mail(data)
+
+
+    # class VerifyEmailView(GenericAPIView):
+    # swagger_schema = None
+    # def get(self, request):
+    #     token =request.GET.get('token')
+
+
+
+    #     try:
+    #         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+    #         user = User.objects.get(id=payload['user_id'])
+
+    #         if not user.is_verified:
+    #             user.is_verified = True
+    #             user.save()
+    #         return Response(redirect(f'{settings.BASE_FRONTEND_URL}')) #redirects user to home page
+    #     # if token has expired
+    #     except jwt.ExpiredSignatureError as identifier:
+    #         return Response(
+    #             {'timeout':'token has expired'}
+    #         )
+    #     #if token has been tampered with
+    #     except jwt.exceptions.DecodeError as identifier:
+    #         return Response(
+    #             {'error':'invalid token'}
+    #         )
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import LoginView, RegisterView, VerifyEmailView, GoogleLoginApi, UserApi, UserInitApi
+from .views import LoginView, RegisterView, GoogleLoginApi, UserApi, UserInitApi
 
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
-    path('verify-email', VerifyEmailView.as_view(), name='verify-email'),
     path('signin',LoginView.as_view(), name='signin'),
     path('google-login/', GoogleLoginApi.as_view(),name='google-login' ),
     path('user/', UserApi.as_view(), name='user'),
