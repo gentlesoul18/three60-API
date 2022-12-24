d87bbc347e13066eb267cba86fd9ece060315c2e
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-12-24 01:46:34 +0100

    Modify rigistration error

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,35 +1,50 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
-from django.contrib.auth.password_validation import validate_password
+from three60.utils import PlainValidationError
 from .models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
             required=True,
-            validators=[UniqueValidator(queryset=User.objects.all())]
             )
     username = serializers.CharField(
             required=True,
-            validators=[UniqueValidator(queryset=User.objects.all())]
             )
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(write_only=True, required=True,)
     confirm_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
         fields = ['username',  'email', 'password', 'confirm_password']
+        extra_kwargs = {
+            'email':{'unique':True},
+            'username':{'unique': True},
+        }
 
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['confirm_password']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        """
+        checks if parameters passed in are valid
+        e.g Password length, email uniqueness, et.c
+        
+        """
+        
+        email_exists = User.objects.filter(email=attrs['email']).exists()
+        username_exists = User.objects.filter(username=attrs['username']).exists()
 
+        if username_exists:
+            raise PlainValidationError({"username":"This username exist, please enter a new username"})
+        elif email_exists:
+            raise PlainValidationError({"email":"This email exist, please enter a new email"})
+        elif len(attrs['password']) < 8:
+            raise PlainValidationError({"password": "Password to short, it must contain atleast 8 characters!"})
+        elif attrs['password'] != attrs['confirm_password']:
+            raise PlainValidationError({"password":"Password fields doesn't match."})
         return attrs
 
     def create(self, validated_data):
         user = User.objects.create(
-            username=validated_data['username'],
+            username=validated_data['username'], 
             email=validated_data['email']
         )
 
@@ -37,6 +52,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
 
 
 class UserSerializer(serializers.ModelSerializer):
