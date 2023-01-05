61b9bb7ddee3426fa6a4cdac14e8d7431ef636dd
Gentlesoul <oyebamijibarakat7@gmail.com>
2023-01-05 11:29:36 +0100

    change google login method to post

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import LoginView, RegisterView, GoogleLoginApi, UserApi, CreateUserApi
+from .views import LoginView, RegisterView, GoogleLoginApi ##, UserApi, CreateUserApi
 
 
 urlpatterns = [
     path("register", RegisterView.as_view(), name="register"),
     path("signin", LoginView.as_view(), name="signin"),
     path("google-login", GoogleLoginApi.as_view(), name="google-login"),
-    path("user/", UserApi.as_view(), name="user"),
-    path("create/", CreateUserApi.as_view(), name="create"),
+    # path("user/", UserApi.as_view(), name="user"),
+    # path("create/", CreateUserApi.as_view(), name="create"),
 ]
