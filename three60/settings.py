1739800eba149f116dcf69c2f3bcd32d9f7cfe93
Gentlesoul <oyebamijibarakat7@gmail.com>
2023-03-06 16:01:09 +0100

    Another Method

--- three60/settings.py
+++ three60/settings.py
@@ -34,8 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = env("SECRET_KEY")
 
-SECRET_KEY = config("SECRET_KEY")
-print(SECRET_KEY)
+value = config("SECRET_KEY")
+SECRET_KEY = str(value)
+print(value)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
