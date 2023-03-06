58d088b754d6f0910b86e7545a920106cfa42182
Gentlesoul <oyebamijibarakat7@gmail.com>
2023-03-06 12:38:46 +0100

    Taking status in with  ID

--- todo/serializers.py
+++ todo/serializers.py
@@ -3,7 +3,7 @@ from .models import Todo
 
 
 class TodoSerializer(serializers.ModelSerializer):
-    
+    status = serializers.CharField(required=True)
     class Meta:
         model = Todo
         fields = ["id", "title", "description", "status", "created", "updated"]
