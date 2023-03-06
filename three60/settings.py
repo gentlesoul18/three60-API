97814572a313f10665148e851caaef00d5a8a10b
Gentlesoul <oyebamijibarakat7@gmail.com>
2023-03-06 15:48:17 +0100

    Fixeesss

--- three60/settings.py
+++ three60/settings.py
@@ -34,8 +34,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = env("SECRET_KEY")
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
+SECRET_KEY = config("SECRET_KEY")
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -217,9 +217,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 EMAIL_BACKEND = "gmailapi_backend.mail.GmailBackend"
 # GMAIL_API_CLIENT_ID = env("CLIENT_IID")
-GMAIL_API_CLIENT_ID = os.environ.get("CLIENT_IID")
+GMAIL_API_CLIENT_ID = config("CLIENT_IID")
 # GMAIL_API_CLIENT_SECRET = env("CLIENT_SECRET")
-GMAIL_API_CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+GMAIL_API_CLIENT_SECRET = config("CLIENT_SECRET")
 GMAIL_API_REFRESH_TOKEN = "1//04hJB9E6bLarQCgYIARAAGAQSNwF-L9IrmpAPd6TnqSzUsh2Dh6rZ0dB2J1CqImYgv1FfndxXnwfKZgMjW01VE6t6e7g5b42Y4ao"
 
 
