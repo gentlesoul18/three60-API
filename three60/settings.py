60eb0972336f9f835b8ba520ccc46454fd6ebd0d
Gentlesoul <oyebamijibarakat7@gmail.com>
2023-03-06 15:35:51 +0100

    Fixeesss

--- three60/settings.py
+++ three60/settings.py
@@ -19,7 +19,7 @@ import os
 
 # Initialise environment variables
 # env = environ.Env()
-
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -35,6 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = env("SECRET_KEY")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
