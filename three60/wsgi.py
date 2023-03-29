3904e23798d887001a97bf890bec8469d2f8fc3f
Mukmilatu <muqmilat@gmail.com>
2023-03-29 16:50:25 +0100

    Deploying to render

--- three60/wsgi.py
+++ three60/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "three60.settings")
 
+application = get_wsgi_application()
